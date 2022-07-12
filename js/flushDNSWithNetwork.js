02db754f66ec2019ffab680c601de00cefc9fa11
ShinyNito <82853247+ShinyNito@users.noreply.github.com>
2022-07-12 14:28:31 +0800

    Update flushDNSWithNetwork.js

--- js/flushDNSWithNetwork.js
+++ js/flushDNSWithNetwork.js
@@ -1,12 +1,23 @@
-const network = $network.wifi.ssid;
-const currentTime = Date.now();
-
-
-$httpAPI("GET", "v1/traffic", null, (body) => {
-  if (network && (currentTime / 1000) - body.startTime >= 3) {
-    $httpAPI("post", "v1/dns/flush", null, (body) => {
-      $notification.post('DNS开始刷新', '', `已从蜂窝网络切换至 ${network}`);
-    })
-  }
-  $done();
-})
+(async () => {
+	const network = $network.wifi.ssid;
+	if (network) {
+		const currentTime = Date.now();
+		const wifiEnv = $persistentStore.read("Firewall_Env");
+		if (!wifiEnv) { //if the script was last run in cellular env.
+			const startTime = await new Promise(res => {
+				$httpAPI("GET", "v1/traffic", null, (body) => res(body.startTime))
+			});
+			if ((currentTime / 1000) - startTime >= 3) {
+				const time = JSON.stringify(currentTime);
+				const addTime = $persistentStore.write(time, "WiFi_Timer");
+				const addEnv = $persistentStore.write("1", "Firewall_Env");
+				$httpAPI("post", "v1/dns/flush", null, (body) => {
+        $notification.post('DNS开始刷新', '', `已从蜂窝网络切换至 ${network}`);
+      })
+			}
+		}
+	} else {
+		const delEnv = $persistentStore.write("", "Firewall_Env");
+	}
+})().catch((err) => $notification.post('DNS', '', `出现错误: ${err.message||err}`))
+	.finally(() => $done({}))
