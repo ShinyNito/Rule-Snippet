62db5019d5524c945dd6591ca7ad12100ec45e96
ShinyNito <82853247+ShinyNito@users.noreply.github.com>
2022-07-12 14:26:01 +0800

    Update flushDNSWithNetwork.js

--- js/flushDNSWithNetwork.js
+++ js/flushDNSWithNetwork.js
@@ -1,3 +1,12 @@
-$httpAPI("post", "v1/dns/flush", null, (body) => {
+const network = $network.wifi.ssid;
+const currentTime = Date.now();
+
+
+$httpAPI("GET", "v1/traffic", null, (body) => {
+  if (network && (currentTime / 1000) - body.startTime >= 3) {
+    $httpAPI("post", "v1/dns/flush", null, (body) => {
+      $notification.post('DNS开始刷新', '', `已从蜂窝网络切换至 ${network}`);
+    })
+  }
   $done();
 })
