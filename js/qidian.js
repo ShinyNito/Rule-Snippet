8de2481f878f85e4f86d269aaf50a4053ff4dedc
zhangjie <82853247+ShinyNito@users.noreply.github.com>
2023-05-03 07:48:15 +0800

    Create qidian.js

---
+++ js/qidian.js
@@ -0,0 +1,50 @@
+let url = $request.url;
+let method = $request.method;
+if (!$response.body) {
+    console.log(`$response.body为undefined:${url}`);
+    $done({});
+}
+
+let body = JSON.parse($response.body);
+
+const noticeTitle = "起点App脚本错误";
+const getMethod = "GET";
+const postMethod = "POST";
+
+if (!body.Data) {
+    console.log(`body:${$response.body}`);
+    $notification.post(noticeTitle, "起点", "Data为空");
+} else {
+    if (url.includes("v1/adv/getadvlistbatch?positions=iosusercenter%2Ciosusercenter2") && method === getMethod) {
+        console.log('起点-iOS_用户中心');
+        // 将body.Data的每一个数组元素配置为空 {} body.Data = {};
+        body.Data.iosusercenter = [];
+        body.Data.iosusercenter2 = [];
+
+        console.log('成功');
+    } else if (url.includes("v3/user/getaccountpage") && method === getMethod) {
+        console.log('起点-账户页');
+        if (!body.Data) {
+            console.log(`body:${$response.body}`);
+            $notification.post(noticeTitle, "起点", "Data为空");
+        }
+        body.Data.BenefitButtonList = [];
+        const newFunctionButtonList = [];
+        const excludeList = ['游戏中心', '徽章称号', '新书投资', '达人中心', '奇妙世界', '数字藏品'];
+        body.Data.FunctionButtonList.forEach(element => {
+            if (!excludeList.includes(element.Name)) {
+                newFunctionButtonList.push(element);
+              }
+        });
+        body.Data.FunctionButtonList = newFunctionButtonList;
+        //删除SchoolText
+        body.Data.SchoolText = "";
+        //删除Member
+        body.Data.Member = {};
+    }
+}
+body = JSON.stringify(body);
+
+$done({
+    body
+});
