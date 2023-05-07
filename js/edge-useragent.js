b3e448796b8067a04a2c2860f7effca09e2c5360
zhangjie <82853247+ShinyNito@users.noreply.github.com>
2023-05-08 03:51:38 +0800

    Update edge-useragent.js

--- js/edge-useragent.js
+++ js/edge-useragent.js
@@ -5,13 +5,10 @@
   const platform = headers['sec-ch-ua-platform']
   const userAgent = headers['user-agent']
   delete headers['user-agent']
-  delete headers['sec-ch-ua-full-version']
-  delete headers['sec-ch-ua-full-version-list']
   const windowsUa = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.35'
   const linuxUa = 'Mozilla/5.0 (Linux; Android 10; Redmi K30 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.35'
   const androidUa = 'Mozilla/5.0 (Linux; Android 10; Redmi K30 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.35'
   const iosUa = 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) EdgiOS/112.0.1722.64 Version/16.0 Mobile/15E148 Safari/604.1'
-  const secChUa = '"Microsoft Edge";v="113", "Chromium";v="113", "Not-A.Brand";v="24"'
   headers['sec-ch-ua'] = secChUa
   // console.log(userAgent)
   if (platform === "Windows") {
