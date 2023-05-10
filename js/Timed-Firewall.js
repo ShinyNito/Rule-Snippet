96c4289ae1f06793fda73f7ee038aaa032473d7c
zhangjie <82853247+ShinyNito@users.noreply.github.com>
2023-05-11 02:19:55 +0800

    Update Timed-Firewall.js

--- js/Timed-Firewall.js
+++ js/Timed-Firewall.js
@@ -7,7 +7,7 @@ let block = {
 if (readTimer) {
   const currentTime = Date.now();
   const markTime = parseInt(readTimer);
-  if (currentTime - markTime <= 15000) {
+  if (currentTime - markTime <= 10000) {
     block.matched = true;
     const addNum = JSON.stringify(parseInt(record || "0") + 1);
     const saveNum = $persistentStore.write(addNum, "Block_Num");
