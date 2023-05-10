6e72c3720712ce24803747a549c160d0926b4e6c
zhangjie <82853247+ShinyNito@users.noreply.github.com>
2023-05-11 02:01:16 +0800

    Update WiFi-Firewall.js

--- js/WiFi-Firewall.js
+++ js/WiFi-Firewall.js
@@ -2,7 +2,7 @@ const network = $network.wifi.ssid;
 const currentTime = Date.now();
 
 $httpAPI("GET", "v1/traffic", null, (body) => {
-  if (network && (currentTime / 1000) - body.startTime >= 3) {
+  if (network && (currentTime / 1000) - body.startTime >= 10) {
     const time = JSON.stringify(currentTime);
     const addTime = $persistentStore.write(time, "WiFi_Timer");
   }
